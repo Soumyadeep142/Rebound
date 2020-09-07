94a355a887d6b02906b41c63ab05b0f0f11e87b7
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2020-09-08 03:13:35 +0530

    Update Solar_System.py

--- Solar_System.py
+++ Solar_System.py
@@ -5,19 +5,15 @@ from numpy import *
 sim = rebound.Simulation()
 sim.units=('kg','m','s')
 
-#Searching Nasa Horizons
-sim.add(particle="Sun") 
-sim.add(particle="Mercury")
-sim.add(particle="Venus")
-sim.add(particle="Earth") 
-sim.add(particle="Mars")
-sim.add(particle="Jupiter")
-sim.add(particle="Saturn")
-sim.add(particle="Uranus")
-sim.add(particle="Neptune")
+
+sim.add(particle='Sun')
+plannet=['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune']
+for particles in plannet:
+	sim.add(particle=particles)
 sim.integrate(2*pi)
 
-fig,ax_main=rebound.OrbitPlot(sim, xlim=(-6*10**12,6*10**12), ylim=(-5*10**12,5*10**12), unitlabel='km', color=['grey','brown',"blue","red",'orange','yellow','black','green'])
+fig,ax_main=rebound.OrbitPlot(sim, xlim=(-6*10**12,6*10**12), ylim=(-5*10**12,5*10**12), unitlabel='km', color=['grey','pink',"blue","red",'orange','yellow','white','green'], orbit_type='solid', fancy='true')
 fig.savefig('Solar System.pdf')
 fig.show()
+
 
