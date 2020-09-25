bd9e6bbf42005d964357721da9bf8282351fce4a
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2020-09-26 03:47:47 +0530

    Add files via upload

--- Solar_System.py
+++ Solar_System.py
@@ -1,23 +1,28 @@
 import rebound
 from numpy import *
-from matplotlib import *
+from matplotlib.pyplot import *
 
-sim = rebound.Simulation()
+sim=rebound.Simulation()
 sim.units=('kg','km','yr')
+sim.integrator='whfast'
+sim.add(particle="Sun")
 
-#Searching Nasa Horizons
-
-sim.add(particle='Sun')
-plannet=['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
+plannet=['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune', 'Pluto']
 for particles in plannet:
-	sim.add(particle=particles)
-year=linspace(1,200,1500)
+	if particles=='Jupiter':
+		sim.add(m=1.898580250761156e29 ,a=778480939.193071 ,e=0.048650024931482155)
+	else:
+		sim.add(particle=particles)
+sim.status()
+sim.move_to_com()
+
+year=arange(1,1000,1)
 j=0
 for t in year:
 	j+=1
 	sim.integrate(t)
-	fig,ax_main=rebound.OrbitPlot(sim, xlim=(-6*10**9,8*10**9), ylim=(-5*10**9,7*10**9), unitlabel='km', color=['grey','pink',"blue","red",'orange','yellow','white','green','grey'], orbit_type='solid', fancy='true')
+	fig,ax_main=rebound.OrbitPlot(sim, xlim=(-7*10**9,7*10**9), ylim=(-7*10**9,7*10**9), unitlabel='km', color=['grey','pink',"blue","red",'orange','yellow','white','green','cyan'], orbit_type='solid', fancy='true')
+	title('year={0}'.format(round(t,1)))
 	fig.savefig('Solar System{0}.png'.format(j))
 	print(t)
 fig.show()
-
